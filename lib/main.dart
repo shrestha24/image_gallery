17b650f8d352b77430307a22197dc87fad7169b0
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2020-09-21 20:18:35 +0530

    Image from phone Image Gallery.

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:file_picker/file_picker.dart';
 import 'dart:io';
 
 void main() {
   runApp(ImageGallery());
 }
 
-class ImageGallery extends StatefulWidget {
-  ImageGallery() : super();
-
-  final String title = "Flutter Pick Image demo";
-
-  @override
-  _imageGallery createstate() => _imageGallery();
-
-}
-
- class _imageGallery extends State<ImageGallery> {
-
-  Future<File> imageFile;
-
-  pickImageFromGallery(ImageSource source){
-    imageFile = ImagePicker.pickImage(source: source);
-  }
-
-  Widget showImage() {
-    return FutureBuilder<File> (
-      future: imageFile,
-      builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
-        if (snapshot.connectionState == ConnectionState.done &&
-        snapshot.data != null) {
-          return Image.file(
-              snapshot.data,
-          width: 300,
-          height: 300,
-          );
-        } else if (snapshot.error != null) {
-          return const Text(
-            'Error Picking Iamge',
-            textAlign: TextAlign.center,
-          );
-        } else {
-          return const Text(
-              'No Image Selected',
-            textAlign: TextAlign.center,
-          );
-        }
-      },
-    );
-  }
+class ImageGallery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          showImage(),
-            RaisedButton(
-              child: Text("Slet Image from Gallery"),
-              onPressed: () {
-                pickImageFromGallery(ImageSource.gallery);
-              },
-            )
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: RaisedButton(
+            child: Text(
+              'Click Here'
+            ),
+            onPressed: () async{
+              FilePickerResult result = await FilePicker.platform.pickFiles();
+
+              if(result != null) {
+                PlatformFile file = result.files.first;
+
+                print(file.name);
+                print(file.bytes);
+                print(file.size);
+                print(file.extension);
+                print(file.path);
+              }
+            },
+          ),
         ),
       ),
     );
   }
- }
+
+}
